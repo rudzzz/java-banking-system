ae267cc0adeb478a36e8ff2e690f0f9c1a3ad5ff
rudzzz <rudsolza13@gmail.com>
2022-10-03 20:46:10 -0300

    Added deposit and withdrawal functions

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,35 @@
-
 public class Account {
-
+	int balance;
+	int previousTransaction;
+	String name;
+	String id;
+	
+	// constructor of class
+	Account(String customerName, String customerId){
+		name = customerName;
+		id = customerId;
+	}
+	
+	// function to deposit money in the account
+	void deposit(int amount) {
+		if(amount != 0) {
+			balance += amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	// function to withdraw money from account
+	void withdraw(int amount) {
+		if(amount != 0) {
+			balance += balance - amount;
+			previousTransaction = -amount;
+		}
+	}
+	
+	// function to show the previous transaction made on the account
+	void getPreviousTransaction() {
+		
+	}
 }
+
+	
